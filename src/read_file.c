d5ac41d9cf303d948fd19173db120739c2a954ba
kane <kane@kane>
2019-03-17 16:44:10 +0100

    update

--- src/read_file.c
+++ src/read_file.c
@@ -11,6 +11,8 @@ int		read_file(char *filename, char **str_tetri, int *tetri_counter)
 		return (-1);
 	while((ret = read(fd, buffer, 21)))
 	{
+		if(ret != 21 && ret != 20)
+			return (-1);
 		buffer[19] = '\0';
 		str_tetri[*tetri_counter] = (char *)malloc(sizeof(char) * 20);
 		if(!str_tetri)
