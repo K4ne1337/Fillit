fc60df6a8286807b27894b2a66b6cf296b9ebeef
Arthur BELKHAYAT <abelkhay@e1r2p19.42.fr>
2019-03-18 17:16:32 +0100

    update final

--- src/read_file.c
+++ src/read_file.c
@@ -6,34 +6,33 @@
 /*   By: abelkhay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:38:45 by abelkhay          #+#    #+#             */
-/*   Updated: 2019/03/18 15:38:47 by abelkhay         ###   ########.fr       */
+/*   Updated: 2019/03/18 17:11:33 by abelkhay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "read_file.h"
 
-int		read_file(char *filename, char **str_tetri, int *tetri_counter)
+int	read_file(char *filename, char **str_tetri, int *tetri_counter)
 {
 	int		fd;
 	int		ret;
 	char	buffer[22];
-	int temp;
+	int		temp;
 
 	fd = open(filename, O_RDONLY);
-	if(fd == -1)
+	if (fd == -1)
 		return (-1);
-	while((ret = read(fd, buffer, 21)))
+	while ((ret = read(fd, buffer, 21)))
 	{
-		if(ret != 21 && ret != 20)
+		if (ret != 21 && ret != 20)
 			return (-1);
 		buffer[ret] = '\0';
-		str_tetri[*tetri_counter] = ft_strdup(buffer);
-		if(!str_tetri)
+		if (!(str_tetri[*tetri_counter] = ft_strdup(buffer)))
 			return (-1);
-		if(verify_block(str_tetri[*tetri_counter]) == -1)
+		if (verify_block(str_tetri[*tetri_counter]) == -1)
 			return (-1);
 		(*tetri_counter)++;
-		if(*tetri_counter > 26)
+		if (*tetri_counter > 26)
 			return (-1);
 		temp = ret;
 	}
--- src/read_file.h
+++ src/read_file.h
@@ -6,20 +6,19 @@
 /*   By: abelkhay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:38:52 by abelkhay          #+#    #+#             */
-/*   Updated: 2019/03/18 15:40:52 by abelkhay         ###   ########.fr       */
+/*   Updated: 2019/03/18 17:14:40 by abelkhay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _READ_FILE_H_
-# define _READ_FILE_H_
+#	ifndef READ_FILE_H
+#	define READ_FILE_H
 
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
-# include "verify_block.h"
-# include "../libft/libft.h"
+#include "verify_block.h"
+#include "../libft/libft.h"
 
 int		read_file(char *filename, char **str_tetri, int *tetri_counter);
 
-#endif
+#	endif
