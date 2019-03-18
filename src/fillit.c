956e98feeaefced5dc42705a1f99f66beff00478
Arthur BELKHAYAT <abelkhay@e1r2p19.42.fr>
2019-03-18 17:08:17 +0100

    update final

--- src/fillit.c
+++ src/fillit.c
@@ -6,7 +6,7 @@
 /*   By: abelkhay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:37:58 by abelkhay          #+#    #+#             */
-/*   Updated: 2019/03/18 15:38:08 by abelkhay         ###   ########.fr       */
+/*   Updated: 2019/03/18 17:05:10 by abelkhay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 int	main(int argc, char **argv)
 {
-	char	**str_tetri;
-	TetriminoList tetri_list;
-	
+	char			**str_tetri;
+	t_tetriminolist	tetri_list;
+
 	str_tetri = NULL;
 	tetri_list.list = NULL;
 	tetri_list.size = 0;
-
-	if(argc != 2)
+	if (argc != 2)
 	{
 		ft_putendl("usage: missing arg");
-		return(1);
+		return (1);
 	}
 	str_tetri = (char **)malloc(sizeof(char *) * 26);
 	if (read_file(argv[1], str_tetri, &tetri_list.size) == -1)
@@ -32,8 +31,9 @@ int	main(int argc, char **argv)
 		ft_putendl("error");
 		exit(1);
 	}
-	tetri_list.list = (Tetrimino *)malloc(sizeof(Tetrimino) * tetri_list.size);
-	fillList(&tetri_list, str_tetri);
+	tetri_list.list = (t_tetrimino *)\
+					malloc(sizeof(t_tetrimino) * tetri_list.size);
+	ft_filllist(&tetri_list, str_tetri);
 	solve(&tetri_list);
 	free(str_tetri);
 	free(tetri_list.list);
--- src/fillit.h
+++ src/fillit.h
@@ -6,17 +6,17 @@
 /*   By: abelkhay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:38:15 by abelkhay          #+#    #+#             */
-/*   Updated: 2019/03/18 15:40:12 by abelkhay         ###   ########.fr       */
+/*   Updated: 2019/03/18 17:06:34 by abelkhay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _FILLIT_H_
-# define _FILLIT_H_
+#	ifndef FILLIT_H
+#	define FILLIT_H
 
-# include "read_file.h"
-# include "tetrimino.h"
-# include "ft_solve.h"
+#include "read_file.h"
+#include "tetrimino.h"
+#include "solve.h"
 
 void	fillit(char *filename);
 
-#endif
+#	endif
